46792118185ea1d910aa352dd80d28b20f735edf
vovaprodan <121820923+vovaprodan@users.noreply.github.com>
2023-05-26 21:49:27 +0200

    ghgj

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,39 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const pictureMarkup = createPictureCards(galleryItems);
+// console.log(galleryItems);
+gallery.insertAdjacentHTML("beforeend", pictureMarkup)
+
+gallery.addEventListener('click', onClickCard)
+
+function createPictureCards(galleryItems) {
+    const markup = galleryItems.map(({preview, original, description}) => {
+        return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    }).join('')
+    return markup;
+}
+function onClickCard(event) {
+    event.preventDefault();
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,40 @@
+import Player from '@vimeo/player'
+const iframe = document.querySelector('iframe');
 
+
+    const player = new Player(iframe);
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+//     const onPlay = function(data) {
+//    console.log( 'час');
+// };
+
+// player.on('timeupdate', onPlay);
+
+// localStorage.setItem("videoplayer-current-time", JSON.stringify(player.on('timeupdate', onPlay)))
+player.on('timeupdate', function(data) {
+  const currentTime = data.seconds;
+  localStorage.setItem("videoplayer-current-time", currentTime);
+});
+
+player.setCurrentTime(second).then(function(seconds) {
+    const second = localStorage.getItem("videoplayer-current-time");
+    console.log(second)
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
